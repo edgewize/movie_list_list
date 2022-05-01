e111bd7065d88167f359b75ab3020a83075ddd7d
taylor edginton <tedginton88@gmail.com>
2022-05-01 14:47:41 -0600

    Check point

--- refresh_db.py
+++ refresh_db.py
@@ -1,3 +1,13 @@
 from project import db, create_app, models
+import datetime
 
 db.create_all(app=create_app())
+
+first_list = models.FilmList(
+    title="First Movie List",
+    description="A fun collection of favorite films",
+    created_at=datetime.datetime.now().date(),
+    film_list="218,348",
+)
+db.session.add(first_list)
+db.session.commit()
